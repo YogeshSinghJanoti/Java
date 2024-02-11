202f281f335f78801ffe262b642f7e2cc2f6f325
YogeshSinghJanoti <137821794+YogeshSinghJanoti@users.noreply.github.com>
2024-02-11 08:04:11 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,24 @@
+public class Literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int Age = 33;
+        short num = 23;
+        long number = 23233434L;
+        char ch = 'A';
+        float fl = 5.6f;
+        double dl = 4.66;
+        boolean a = true;
+        String name = "yogesh";
+
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(fl);
+        System.out.println(dl);
+        System.out.println(Age);
+        System.out.println(number);
+        System.out.println(num);
+        System.out.println(a);
+        System.out.println(name);
+
+    }
+}
