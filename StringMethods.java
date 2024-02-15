5a9deab2cf860f776ff4a327f583df1eb3568ec4
Yogesh Singh Janoti <137821794+YogeshSinghJanoti@users.noreply.github.com>
2024-02-15 09:35:04 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,43 @@
+import java.sql.SQLOutput;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String name = "Singh";
+        System.out.println(name);
+
+        //int value = name.length();  // Total number of characters in the string.
+        //System.out.println(value);
+
+        //String nameLowerCase = name.toLowerCase();  // Change the String to lower case.
+        //System.out.println(nameLowerCase);
+
+        //String nameUpperCase = name.toUpperCase();  // Change the String to UPPER case.
+        //System.out.println(nameUpperCase);
+
+        //String nonTrimmedString = "      yogesh     ";
+        //System.out.println(nonTrimmedString);
+        //String trimmedString = nonTrimmedString.trim();  // To removes the whitespaces in the String.
+        //System.out.println(trimmedString);
+
+        //System.out.println(name.substring(2));  // To start the String from index 2.
+        //System.out.println(name.substring(1,4));  // To start the String from index 1 till 4.
+
+        //System.out.println(name.replace('S','K'));  // To replace a single character in the String with the other.
+        //System.out.println(name.replace("ngh","ncere"));  // To replace many characters in the String with the other.
+
+        //System.out.println(name.startsWith("Sin"));  // To check if the name starts with the given characters.
+        //System.out.println(name.endsWith("h"));  // To check if the name ends with the given characters.
+
+        //System.out.println(name.charAt(1));  // Character at that index.
+        //System.out.println(name.indexOf('n'));  // Index at which the character is present.
+
+        //String modifiedName = "Ritsrits";
+        //System.out.println(modifiedName.indexOf("ts",3));  // To find the index of the substring.
+        //System.out.println(modifiedName.lastIndexOf("it"));  // To find the index of last occurrence of the substring.
+        //System.out.println(modifiedName.lastIndexOf("it",4));  // To find the last occurrence of the substring up to the index n.
+
+        //System.out.println(name.equals("Singh"));  // To check if the String equals to the given String.
+        //System.out.println(name.equalsIgnoreCase("sINgh"));  // To check if the String equals , but it ignores the case of the String.
+
+    }
+}
